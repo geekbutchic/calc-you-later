03e32e4c877ae6bcbe5f075d23d42ff3edec8851
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-22 14:50:18 -0400

    initial atempt

---
+++ calculate-front-end.js
@@ -0,0 +1,22 @@
+const calculate = require ('./calculate-back-end.js');
+
+
+const calculate = process.argv[2]
+
+
+console.log(calculate)
+return calculate;
+
+
+
+// I tried but cannot understand just exactly how to 
+// even get a consol.log(printing)
+// Every time we go over it, seems much easier then expected
+
+
+
+
+
+
+
+
